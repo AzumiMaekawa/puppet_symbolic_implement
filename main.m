98fc6428ee7b906509ef82b9af94bd568fa928ce
AzumiMaekawa <applle1111@gmail.com>
2019-10-13 00:01:42 +0900

    update (Newton's method does not converge depending on initial state

--- main.m
+++ main.m
@@ -42,27 +42,26 @@ x_im1_sym = sym('x%d_m1', [3*n 1]);
 x_im2_sym = sym('x%d_m2', [3*n 1]);
 
 %%  compute the time-discretized acceleration
-% xdot_i = (x_i_sym - x_im1_sym) / h;
 xdot2_i = (x_i_sym - 2 * x_im1_sym + x_im2_sym) / (h^2);
 
 %%  internal potential deformation energy (strings and trusses)
 W = compute_potential_energy(x_i_sym, p_i_sym);
 
 % calculate x_i with implicit Euler time stepping scheme
-global U_i gradU hessU;
-% U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + W + g.' * M * x_i_sym;
-U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + g.' * M * x_i_sym;% + W + g.' * M * x_i_sym;
+ U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + W + g.' * M * x_i_sym;
+% U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + g.' * M * x_i_sym;% + W + g.' * M * x_i_sym;
 %%  compute gradient and hessian
+global gradU hessU;
 gradU = jacobian(U_i, x_i_sym);
 gradU = gradU(:);
 hessU = jacobian(gradU, x_i_sym);
 
 %% Main Processing 
 % Initialize
-x_0 = zeros([3*n 1])+[0;0;0.1];
+x_0 = zeros([3*n 1]);
 xdot_0 = zeros([3*n 1]);
-% p_x = linspace(0, 0.5, n_p*T);
-p_x = zeros([1 n_p*T]);
+p_x = linspace(0, 0.5, n_p*T);
+%p_x = zeros([1 n_p*T]);
 p_y = zeros([1 n_p*T]);
 p_z = zeros([1 n_p*T]) + s_0(1);
 p = [p_x; p_y; p_z];
@@ -81,6 +80,10 @@ scatter3(x(1,1), x(2,1), x(3,1), 15,'r','filled'); % position, size, color, fill
 scatter3(x(1,n*T), x(2,n*T), x(3,n*T),15,'k','filled');
 % draw control trajectory
 plot3(p_x, p_y, p_z,'Color', 'g'); % plot control position with green line
+for i = 1:T
+    plot3([p_x(i) x(1,i)], [p_y(i) x(2,i)], [p_z(1) x(3,i)],'Color','k');
+end
+
 title('the simulated location');
 xlabel('x'); ylabel('y'); zlabel('z');
 % xlim([-1 1]); ylim([-1 1]); zlim([-1 1]);
@@ -91,8 +94,6 @@ function x = forward_sim(p, x_0, xdot_0)
     x_i = x_0;
     x_im1 = x_0;
     x_im2 = x_0;
-    % xdot_i = xdot_0;
-    % xdot_im1 = xdot_0;
 
     x = x_0;
     for i = 1:(T-1)
@@ -100,22 +101,19 @@ function x = forward_sim(p, x_0, xdot_0)
         grad_i = subs(gradU, p_i_sym, p_i);
         grad_i = subs(grad_i, x_im1_sym, x_im1);
         grad_i = subs(grad_i, x_im2_sym, x_im2);
-%        hess_i = subs(hessU, p_i_sym, p_i);
-%        hess_i = subs(hess_i, x_im1_sym, x_im1);
-%        hess_i = subs(hess_i, x_im2_sym, x_im2);
+        hess_i = subs(hessU, p_i_sym, p_i);
+        hess_i = subs(hess_i, x_im1_sym, x_im1);
+        hess_i = subs(hess_i, x_im2_sym, x_im2);
 
         % compute x_i with Newton's method
         fprintf('forward_sim :: %d/%d\n', i, T);
         % simplified one to see if it can be converged or not.
-%        x_opt = Newtons_method(grad_i, hess_i, x_i, 1e-5, 20); %% -> succeeded to converge
-        x_opt = gradient_descent(grad_i, x_i, 1e-5, 50);
-        x = [x; x_opt];
+        %x_i = Newtons_method(grad_i, hess_i, x_i, 1e-5, 20); %% -> succeeded to converge
+        x_i = gradient_descent(grad_i, x_i, 1e-5, 50);
+        x = [x; x_i];
 
         x_im2 = x_im1;
-        x_im1 = x_opt;
-        x_i = x_opt;
-        % xdot_im1 = xdot_i;
-        % xdot_i = (x_i - x_im1_sym) / h;
+        x_im1 = x_i;
     end
 end
 
@@ -131,14 +129,9 @@ function x_opt = Newtons_method(grad_i, hess_i, x_init, err, max_iteration)
     cnt = 0;
     while(1)
         cnt = cnt + 1;
-%        disp(x_k);
         fprintf('Newtons method :: %d/%d\n', cnt, max_iteration)
         grad_k = subs(grad_i, x_i_sym, x_k);
         hess_k = subs(hess_i, x_i_sym, x_k);
-%        grad_k = double(grad_k);
-%        hess_k = double(hess_k);
-%        disp(grad_k);
-%        disp(hess_k);
         dx = double(-inv(hess_k + r*I) * grad_k);
         disp(dx);
         x_k = double(x_k + dx);
