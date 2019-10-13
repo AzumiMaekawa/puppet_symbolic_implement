4ed5d6ee90443eaeac3df03caa1c5f169cf4c351
Azumi Maekawa <applle1111@gmail>
2019-10-13 20:21:28 +0900

    update & implement objective function

--- compute_potential_energy.m
+++ compute_potential_energy.m
@@ -1,5 +1,5 @@
 function W = compute_potential_energy(x_i, p_i)
-global n n_s k s_0 psy debug;
+global n n_s k s_0 psy epsilon debug;
 
 % W_puppet: the energy stored in the stiff springs (puppet linkages)
 W_puppet = 0;
@@ -13,6 +13,11 @@ end
 
 % W_string: the energy stored in the strings
 % psy: model of a string, piece-wise function
+syms psy(s);
+psy(s) = piecewise(0<s, (1/2)*s^2 + (epsilon/2)*s + epsilon^2 / 6,...
+            -epsilon<s<0, (1/(6*epsilon))*s^3 + (1/2)*s^2 + (epsilon/2)*s + epsilon^2/6, ...
+            0);
+
 W_string = 0;
 for i = 1:n_s
     % sym tmp_f; % being local to be readable
--- main.m
+++ main.m
@@ -4,13 +4,13 @@ clc;
 
 %% Init to prepare the values to simualte
 global n n_p n_s m M g T plannningHorizon h s_0 k epsilon stringsPairs debug;
-n = 1; % Pendulum casea
+n = 1; % number of mass points. n = 1 means a pendulum case
 n_p = 1; % number of attachment points
 n_s = 1; % number of strings
 m = 1.0;
-M = eye(3) * m; % Mass matrix
+M = eye(3*n) * m; % Mass matrix
 g = [0; 0; 9.81];
-T = 40; % "The number of time steps was 40 in all cases"
+T = 40; % "The number of time steps was 40 in all cases" from the paper
 plannningHorizon = 1.0; % [s]
 h = plannningHorizon / T; % step size
 debug = false; % Enable debug mode to see what's happening
@@ -21,12 +21,8 @@ epsilon = 0.001;
 stringsPairs = reshape([1 1], [2, n_s]);
 % linkPairs = reshape([1 2 2 3], [2,2]);
 
-%% String model creation
-global psy p_i_sym x_i_sym x_im1_sym x_im2_sym;
-syms psy(s);
-psy(s) = piecewise(0<s, (1/2)*s^2 + (epsilon/2)*s + epsilon^2 / 6,...
-            -epsilon<s<0, (1/(6*epsilon))*s^3 + (1/2)*s^2 + (epsilon/2)*s + epsilon^2/6, ...
-            0);
+%% Compute gradient and hessian for forward simulation
+global psy p_i_sym x_i_sym x_im1_sym x_im2_sym gradU hessU;
 
 if(debug)
     idx = linspace(-5,5,200);
@@ -41,16 +37,15 @@ p_i_sym = sym('p%d', [3*n_p 1]);
 x_im1_sym = sym('x%d_m1', [3*n 1]);
 x_im2_sym = sym('x%d_m2', [3*n 1]);
 
-%%  compute the time-discretized acceleration
+% compute the time-discretized acceleration
 xdot2_i = (x_i_sym - 2 * x_im1_sym + x_im2_sym) / (h^2);
 
-%%  internal potential deformation energy (strings and trusses)
+% internal potential deformation energy (strings and trusses)
 W = compute_potential_energy(x_i_sym, p_i_sym);
 
-% calculate x_i with implicit Euler time stepping scheme
- U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + W + g.' * M * x_i_sym;
-%%  compute gradient and hessian
-global gradU hessU;
+% To calculate x_i with implicit Euler time stepping scheme
+U_i = (h^2 / 2) * xdot2_i.' * M * xdot2_i + W + g.' * M * x_i_sym;
+% compute gradient and hessian of U_i
 gradU = jacobian(U_i, x_i_sym);
 gradU = gradU(:);
 hessU = jacobian(gradU, x_i_sym);
@@ -59,18 +54,31 @@ hessU = jacobian(gradU, x_i_sym);
 % Initialize
 x_0 = randn([3*n 1]) * 0.001;
 x_m1 = zeros([3*n 1]);
-t = linspace(0, 1, n_p*T);
+t = linspace(0, plannningHorizon, T);
 % p_x = t.*t;
-p_x = linspace(0, 0.5, n_p*T);
-p_y = sin(t*pi/2);
-%p_y = zeros([1 n_p*T]);
-p_z = zeros([1 n_p*T]) + s_0(1);
+p_x = linspace(0, 0.5, T);
+%p_y = sin(t*pi/2);
+p_y = zeros([1 T]);
+p_z = zeros([1 T]) + s_0(1);
 p = [p_x; p_y; p_z];
 p = p(:);
+% specify target trajecory
+syms f(s);
+f(s) = 519*s^7 + -1471*s^6 + 1632*s^5 - 900.6*s^4 + 265.1*s^3 - ...
+        44.22*s^2 + 4.268*s + 0.00026816;
+target_x = t;
+target_y = zeros([1 T]);
+target_z = double(f(t));
+x_target = [target_x;target_y;target_z];
+x_target = x_target(:);
+
 
 x = forward_sim(p, x_0, x_m1);
+O = compute_objective(x, x_target, p, false);
+disp(O);
 
-% show results
+
+%% show results
 x = reshape(x, [3,n*T]);
 figure; scatter3(x(1,:),x(2,:),x(3,:)); 
 hold on;
@@ -87,7 +95,7 @@ end
 
 title('the simulated location');
 xlabel('x'); ylabel('y'); zlabel('z');
-% xlim([-1 1]); ylim([-1 1]); zlim([-1 1]);
+ xlim([-0.25 0.75]); ylim([-0.5 0.5]); zlim([-0.5 0.5]);
 
 %% forward simulation x(p)
 function x = forward_sim(p, x_0, x_m1)
@@ -109,11 +117,40 @@ function x = forward_sim(p, x_0, x_m1)
         % compute x_i with Newton's method or gradient descent
         fprintf('forward_sim :: %d/%d\n', i, T);
         % simplified one to see if it can be converged or not.
-        x_i = Newtons_method(grad_i, hess_i, x_i, 1e-4, 20); %% -> succeeded to converge
+        x_i = Newtons_method(grad_i, hess_i, x_i, 1e-6, 20); %% -> succeeded to converge
         % x_i = gradient_descent(grad_i, x_i, 1e-5, 50);
         x = [x; x_i];
 
         x_im2 = x_im1;
         x_im1 = x_i;
     end
+end
+
+function O = compute_objective(x, x_target, p, periodic)
+    global h n_p T
+
+    % Compute O_traj
+    % O_traj measures the similarity between x and the user specified trajectory: x_target.
+    O_traj = norm(x - x_target)^2;
+
+    % Compute O_acc
+    % O_acc regularizes the acceleration of the string attachment points to promote smooth motion.
+    % compute time-discretized acceleration of p
+    % pdot_0 = 0, pdot2_0 = 0
+    pdot = zeros([3*n_p*T 1]);
+    pdot(3*n_p+1:3*n_p*T) = (1/h) * (p(3*n_p+1:3*n_p*T) - p(1:3*n_p*(T-1)));
+    pdot2 = zeros([3*n_p*T 1]);
+    pdot2(3*n_p+1:3*n_p*T) = (1/h) * (pdot(3*n_p+1:3*n_p*T) - p(1:3*n_p*(T-1)));
+
+    O_acc = norm(pdot2)^2;
+
+    % Compute O_CycPos
+    % for a periodic motion
+    O_CycPos = 0;
+    if periodic == true
+        % TODO 
+    end
+
+    % return objective
+    O = O_traj + O_acc + O_CycPos;
 end
