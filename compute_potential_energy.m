4ff10e4ec01e0a29dbf4e17ba23d98ebb249e68b
AzumiMaekawa <applle1111@gmail.com>
2019-10-23 01:05:06 +0900

    update

--- compute_potential_energy.m
+++ compute_potential_energy.m
@@ -1,4 +1,4 @@
-function W = compute_potential_energy(x_i, p_i)
+function W = compute_potential_energy(xi, pi)
 global n n_s k s_0 psy epsilon debug;
 
 % W_puppet: the energy stored in the stiff springs (puppet linkages)
@@ -21,7 +21,7 @@ psy(s) = piecewise(0<s, (1/2)*s^2 + (epsilon/2)*s + epsilon^2 / 6,...
 W_string = 0;
 for i = 1:n_s
     % sym tmp_f; % being local to be readable
-    tmp_f = norm(x_i - p_i) - s_0(i);
+    tmp_f = norm(xi - pi) - s_0(i);
     W_string = W_string + k * psy(tmp_f);
 end
 
